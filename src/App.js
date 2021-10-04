d60744b6cac5106478dd71edc26ea2671c530c2f
mfbernasol <mfbernasol@gmail.com>
2021-10-04 16:41:46 -0500

    init react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import { render } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    return (
+      <div className='App'>
+        <h1>Hello {name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
