41f8baa38ec68bf049060cdcf2cbbc31438b02b0
mfbernasol <mfbernasol@gmail.com>
2021-10-05 20:46:21 -0500

    create UserItem.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import { render } from '@testing-library/react';
 import { render } from '@testing-library/react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
