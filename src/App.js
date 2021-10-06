af919eafe6b299dd9ccaebc8a43d956e7962390d
mfbernasol <mfbernasol@gmail.com>
2021-10-05 20:14:39 -0500

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import { render } from '@testing-library/react';
 import { render } from '@testing-library/react';
+import Navbar from './components/layout/Navbar';
 import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
   render() {
-    const name = 'John Doe';
     return (
       <div className='App'>
-        <h1>Hello {name}</h1>
+        <Navbar />
       </div>
     );
   }
