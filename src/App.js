8d580df5c2c433c48cccc84bce0c1bc11def8531
mfbernasol <mfbernasol@gmail.com>
2021-10-11 20:50:50 -0500

    changing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import render from 'dom-serializer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const name = 'Bob doe';
+    return (
+      <div className='App'>
+        <h1>Hello From {name}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
