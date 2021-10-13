82cedb58fee2632cedafb3657599fe219253e835
mfbernasol <mfbernasol@gmail.com>
2021-10-13 15:28:54 -0500

    Create Users component, mapped UserItem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import render from 'dom-serializer';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    const name = 'Bob doe';
     return (
       <div className='App'>
         <Navbar />
+        <Users />
       </div>
     );
   }
